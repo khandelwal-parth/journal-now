89664affc53c6c8a6b6573d1f53bf35477615828
khandelwal-parth <parthkhandelwal201107@gmail.com>
2026-05-09 04:34:58 +0530

    fix middleware function name

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { verifyToken } from '@/lib/auth';
 
-export function proxy(request) {
+export function middleware(request) {
   const token = request.cookies.get('journal_token')?.value;
   const { pathname } = request.nextUrl;
 
