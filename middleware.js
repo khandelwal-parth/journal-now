05a1f222f5bd0052d3167b146202c2251aabbfab
khandelwal-parth <parthkhandelwal201107@gmail.com>
2026-05-09 04:38:47 +0530

    disable middleware

--- middleware.js
+++ middleware.js
@@ -1,35 +1,5 @@
-import { NextResponse } from 'next/server';
-import * as jose from 'jose';
-
-export async function middleware(request) {
-  const token = request.cookies.get('journal_token')?.value;
-  const { pathname } = request.nextUrl;
-
-  const isAuthPage = pathname === '/login' || pathname === '/signup';
-  const isApi = pathname.startsWith('/api');
-
-  let user = null;
-  if (token) {
-    try {
-      const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-      const { payload } = await jose.jwtVerify(token, secret);
-      user = payload;
-    } catch {
-      user = null;
-    }
-  }
-
-  if (user && isAuthPage) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
-
-  if (!user && !isAuthPage && !isApi) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
-
-  return NextResponse.next();
-}
+export function middleware() {}
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+  matcher: [],
 };
